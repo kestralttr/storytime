34a5787b15c0a4f65ca4de2bf8c04ac0e0aaed90
Alex Bennett <kestralttr@gmail.com>
2017-04-02 19:08:50 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Story1
+Story.create(title:"It Was A Dark And Stormy Knight",every_word:true,body:"I was out in the fearsome tornado, when suddenly a man asked why he wasn't wearing clothes")
+#Story2
+Story.create(title:"Marshmallows",every_word:true,body:"Graham crackers and chocolate, I said. But we also needed something else.  Something... beefy.")
+#Story3
+Story.create(title:"Delicious Apples",every_word:true,body:"The witch knew that her time was almost over.  She had lived a long life, but without the fruits made out of little children, there wasn't much she could do.")
+#Story4
+Story.create(title:"I Walked Away",every_word:true,body:"I walked away.  And kept walking.  One step after your")
