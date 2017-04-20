5e32af2095f8a7831d0f9f5ff04d1da4eead4c37
Alex Bennett <kestralttr@gmail.com>
2017-04-19 19:10:55 -0700

    updates styling and functionality of error message when users try to add more than one word too quickly - also time limit has been reduced to 15 mins

--- frontend/components/update_story/update_story.jsx
+++ frontend/components/update_story/update_story.jsx
@@ -13,18 +13,21 @@ class UpdateStory extends React.Component {
     this.updateTitle = this.updateTitle.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTrackerKeyMatch = this.handleTrackerKeyMatch.bind(this);
+    this.hideErrorMessage = this.hideErrorMessage.bind(this);
   }
 
   handleTrackerKeyMatch() {
-    let errorMessage = document.getElementById("error-message");
-    errorMessage.innerHTML = "Apologies.  Someone else must contribute before you can add another word.";
+    // let errorMessage = document.getElementById("error-message");
+    // errorMessage.innerHTML = "Apologies.  Someone else must contribute before you can add another word.";
+    $("#update-story-error-container").css("display", "block");
+    $("#update-story-error-container").fadeTo(500,1,"linear");
   }
 
   handleSubmit(e) {
     e.preventDefault();
     if(!Cookies.get('storytimeTrackerKey')) {
       let milliseconds = Math.round(new Date().getTime());
-      Cookies.set('storytimeTrackerKey', milliseconds,{expires: 1});
+      Cookies.set('storytimeTrackerKey', milliseconds,{expires: 1/96});
     }
     let userTrackerKey = Cookies.get('storytimeTrackerKey');
     console.log(userTrackerKey);
@@ -60,6 +63,13 @@ class UpdateStory extends React.Component {
     }
   }
 
+  hideErrorMessage() {
+    $("#update-story-error-container").fadeTo(500,0,"linear");
+    window.setTimeout(() => (
+      $("#update-story-error-container").css("display", "none")
+    ),500);
+  }
+
   updateTitle(e) {
     this.setState({body: e.target.value});
   }
@@ -68,6 +78,11 @@ class UpdateStory extends React.Component {
     console.log(Cookies.get('storytimeTrackerKey'));
     return(
       <div id="update-story-master">
+        <div id="update-story-error-container" onClick={this.hideErrorMessage}>
+          <p>
+            As the most recent contributor to this story, you'll have to wait either 15 minutes, or until another person contributes before you can add another word.  Keep checking back!
+          </p>
+        </div>
         <form>
           <span>Next word (no spaces): </span><input id="add-word-input" type="text"maxLength="50" onChange={this.updateTitle} /><input type="submit" value="Create" onClick={this.handleSubmit}></input><br></br>
           <p id="error-message"></p>
